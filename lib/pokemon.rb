fd989ddc1b0faae54c9dc7615f5720c774f5be9c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-17 04:06:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
   def initialize(id:, name:, type:, hp: nil, db:)
-    attributes.each {|key, value| self.send(("#{key}="), value)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end #initialize
   
   def self.save(name, type, db)
