7c0ff66b363b13fad3c7d871c278f3054b7eb49d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-17 04:20:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
-    db.execute("UPDATE pokemon SET hp=60 WHERE hp = NIL")
+    db.execute("UPDATE pokemon SET hp=60")
   end #initialize
   
   def self.save(name, type, db)
